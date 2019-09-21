79a508ec2c58e5a2929a59da77b720a5ed47ddd2
snobu <foo@snobu.org>
2019-09-21 22:16:56 +0300

    Fix now runs on Python 3

--- qput.py
+++ qput.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/env python3
 
 # How to use the Azure Storage SDK with Python:
 # https://docs.microsoft.com/en-us/azure/storage/storage-python-how-to-use-queue-storage
@@ -7,8 +7,8 @@
 # sudo apt-get install build-essential libssl-dev libffi-dev python-dev
 # sudo pip install azure-storage blessings subprocess
 
-import ConfigParser
-config = ConfigParser.RawConfigParser()
+import configparser
+config = configparser.RawConfigParser()
 config.read('storage.conf')
 accName = config.get('storage_account', 'accName')
 accKey = config.get('storage_account', 'accKey')
@@ -22,25 +22,26 @@ import subprocess
 
 def get_stdout_from_cmd(command):
     try:
-    	p = subprocess.Popen(command,
-        	stdout=subprocess.PIPE,
-        	stderr=subprocess.STDOUT)
-       	stdout = p.stdout.readlines()
+        p = subprocess.Popen(command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT)
+        stdout = p.stdout.readlines()
     except:
         raise Exception('Unable to get_stdout_from_cmd(). Maybe fortune is not installed?')
-    result = ''.join(stdout)
+    result = b''.join(stdout)
     return result
 
 queue_service = QueueService(account_name=accName, account_key=accKey)
 queue_service.encode_function = QueueMessageFormat.text_base64encode
 
 t = Terminal()
-print t.red('Writing to Azure Storage Queue ' + queueName + '...')
+print(t.red('Writing to Azure Storage Queue ' + queueName + '...'))
 
 for i in range (1, 20):
         cmd = 'fortune'
         msg = get_stdout_from_cmd(cmd)
-        queue_service.put_message(queueName, unicode(msg))
+        queue_service.put_message(queueName, msg.decode("utf-8"))
         sys.stdout.write('!')
+        sys.stdout.flush()
 
-print '\nDone adding messages.'
+print('\nDone adding messages.')
--- qread.py
+++ qread.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python -u
+#!/usr/bin/env python3
 
 # How to use the Azure Storage SDK with Python:
 # https://docs.microsoft.com/en-us/azure/storage/storage-python-how-to-use-queue-storage
 
-import ConfigParser
-config = ConfigParser.RawConfigParser()
+import configparser
+config = configparser.RawConfigParser()
 config.read('storage.conf')
 accName = config.get('storage_account', 'accName')
 accKey = config.get('storage_account', 'accKey')
@@ -20,18 +20,18 @@ queue_service = QueueService(account_name=accName, account_key=accKey)
 queue_service.decode_function = QueueMessageFormat.text_base64decode
 
 t = Terminal()
-print t.green('Connected to Azure Storage Queue ' + queueName + '...')
+print(t.green('Connected to Azure Storage Queue ' + queueName + '...'))
 # Get approximate number of messages in queue
 queue_metadata = queue_service.get_queue_metadata(queueName)
 count = queue_metadata.approximate_message_count
-print 'Approximate number of messages in queue: ', count, '\n'
+print('Approximate number of messages in queue: ', count, '\n')
 
 while True:
     messages = queue_service.get_messages(queueName)
     if messages:
         # Get the next message
         for message in messages:
-            print t.bold_yellow(message.content)
+            print(t.bold_yellow(message.content))
             queue_service.delete_message(queueName, message.id, message.pop_receipt)
-            print t.blue('-' * 40 + '\n')
+            print(t.blue('-' * 40 + '\n'))
         time.sleep(4)
